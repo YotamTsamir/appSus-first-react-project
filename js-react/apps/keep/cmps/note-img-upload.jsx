bcba247061354536db940bfb2f049203ab429bbb
Rotem <rotemspivak@gmail.com>
2022-04-28 21:09:29 +0300

    commiting again

---
+++ js-react/apps/keep/cmps/note-img-upload.jsx
@@ -0,0 +1,5 @@
+
+export function NoteImgUpload({event, onImgInput}){
+   
+
+}
--- js-react/apps/keep/cmps/note-preview.jsx
+++ js-react/apps/keep/cmps/note-preview.jsx
@@ -11,43 +11,58 @@ export function NotePreview({note, onDeleteNote}){
     
     if(note.type === "note-txt"){
         return <div className="note" key={note.id}>
-            <button><i className="fa-solid fa-thumbtack"></i></button> 
-            <div className="note-title" >{note.info.title}</div>
+        <div className="thumbtack-btn-container" >
+            <button hidden><i className="fa-solid fa-thumbtack"></i></button> 
+        </div> 
             <div className="note-txt" >{note.info.txt}</div>
+            <div className="trash-btn-container">
             <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+            </div>
         </div>
     }
     if(note.type === "note-img"){
+        console
         return <div className="note" key={note.id}>
-        <button><i className="fa-solid fa-thumbtack"></i></button> 
+        <div className="thumbtack-btn-container" >
+            <button hidden><i className="fa-solid fa-thumbtack"></i></button> 
+        </div>
+        
         <div className="note-title" >{note.info.title}</div> 
-        <img className="note-img" src={note.info.url}></img>
-        <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+        <div className="trash-btn-container">
+            <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+        </div>
     </div>
     }
     if(note.type === "note-video"){
         return <div className="note" key={note.id}>
-        <button><i className="fa-solid fa-thumbtack"></i></button>
-        <div className="note-title" >{note.info.title}</div>
-        <iframe width="420" height="315"
+        <div className="thumbtack-btn-container" >
+            <button hidden><i  className="fa-solid fa-thumbtack"></i></button> 
+        </div>
+        <iframe width="260" height="200"
             src={note.info.url}>
         </iframe>
-        <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+        <div className="note-title" >{note.info.title}</div>
+        <div className="trash-btn-container">
+            <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+        </div>
     </div> 
     }
     if(note.type === "note-todos"){
         const todos = note.info.todos
         return <div className="note" key={note.id}>
-        <button><i className="fa-solid fa-thumbtack"></i></button>
+        <div className="thumbtack-btn-container" >
+            <button hidden><i className="fa-solid fa-thumbtack"></i></button> 
+        </div>
         <div className="note-title" >{note.info.title}</div>
-        <div>Label: {note.info.label}</div>
+        <div>{note.info.label}</div>
         {todos.map((todo, todoIdx) => {                  
-                console.log(todo)
             return <ul key={todoIdx}>
                 <li className="note-todo-txt" >{todo.txt} </li>
             </ul> 
         })}
-        <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+        <div className="trash-btn-container">
+            <button onClick={() => onDeleteNotePreview(note.id)}><i className="fa-solid fa-trash"></i></button>
+        </div>
     </div> 
     }
 }
